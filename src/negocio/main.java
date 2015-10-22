a1d563c0c6a08ba621dcd484865d4d7f27abe192
cmatheus <contato@matheusmaia.com>
2015-10-22 12:57:03 -0300

    deletando visões

---
+++ src/negocio/main.java
@@ -0,0 +1,13 @@
+package negocio;
+
+import javax.swing.JOptionPane;
+
+public class main {
+
+	public static void sair(String[] args) {
+		JOptionPane.showMessageDialog(null, "O sistema será encerrado");
+		System.exit(0);
+		
+	}
+}
+	
