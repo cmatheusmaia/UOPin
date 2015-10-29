4eaacdde29ce8838f976d6f68f096cc3177495e3
cmatheus <contato@matheusmaia.com>
2015-10-29 11:03:50 -0300

    Criada a camada de Cadastrar Evento.

--- src/negocio/managerCadastrarEvento.java
+++ src/negocio/managerCadastrarEvento.java
@@ -1,33 +1,10 @@
 package negocio;
 
-import javax.swing.JOptionPane;
-
-import dao.acesso;
-import view.home;
-<<<<<<< HEAD
-import view.telaCadastroUsuario;
-import view.telaInicial;
-
 public class managerCadastrarEvento {
 
-	
-	public static void chamarRotina(String[] args) {
-		JOptionPane.showMessageDialog(null, "Iniciando tela");
-		new telaCadastroUsuario();
+	public static void chamarRotina(Object object) {
+		// TODO Auto-generated method stub
+		
 	}
-=======
-import view.telaCadastroEvento;
-import view.telaCadastroUsuario;
-import view.telaInicial;
 
-public class managerCadastrarEvento {
-
-	//class para intermediar o dao com a view
-	
->>>>>>> branch 'master' of https://github.com/cmatheusmaia/UOPin.git
 }
-
-
-
-
-
