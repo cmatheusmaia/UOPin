689e4d78f46a74b342628abdb93db3ac0dc3d8ba
cmatheus <contato@matheusmaia.com>
2015-11-08 11:19:54 -0300

    Mudanças

--- src/view/telaCadastroEstabelecimento.java
+++ src/view/telaCadastroEstabelecimento.java
@@ -6,17 +6,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 import negocio.PadroesDeTelas;
-
-import java.awt.Color;
-import javax.swing.JLabel;
-import java.awt.Font;
-import java.awt.SystemColor;
 
 public class telaCadastroEstabelecimento extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
 
 	/**
 	 * Launch the application.
@@ -26,7 +28,6 @@ public class telaCadastroEstabelecimento extends JFrame {
 			public void run() {
 				try {
 					telaCadastroEstabelecimento frame = new telaCadastroEstabelecimento();
-					frame.setLocationRelativeTo(null);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -39,19 +40,57 @@ public class telaCadastroEstabelecimento extends JFrame {
 	 * Create the frame.
 	 */
 	public telaCadastroEstabelecimento() {
-		setTitle("CADASTRAR  ESTABELECIMENTO - 1.0.1");
-		setEnabled(false);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 698, 496);
 		contentPane = new JPanel();
-		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel label = new JLabel("VERS\u00C3O DO SISTEMA 1.0.1 - BUILDER 2015");
-		label.setForeground(SystemColor.controlShadow);
-		label.setFont(new Font("Arial", Font.PLAIN, 10));
-		label.setBounds(221, 437, 231, 23);
-		contentPane.add(label);
+		JLabel lblRazoSocial = new JLabel("Raz\u00E3o social");
+		lblRazoSocial.setBounds(11, 40, 70, 14);
+		contentPane.add(lblRazoSocial);
+		
+		textField = new JTextField();
+		textField.setBounds(75, 37, 380, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblCnpjcpf = new JLabel("CNPJ/CPF");
+		lblCnpjcpf.setBounds(467, 40, 61, 14);
+		contentPane.add(lblCnpjcpf);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(525, 37, 136, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblEndereo = new JLabel("Endere\u00E7o");
+		lblEndereo.setBounds(11, 68, 70, 14);
+		contentPane.add(lblEndereo);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(75, 65, 263, 20);
+		contentPane.add(textField_2);
+		
+		JLabel lblN = new JLabel("Bloco");
+		lblN.setBounds(347, 68, 38, 14);
+		contentPane.add(lblN);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(382, 65, 57, 20);
+		contentPane.add(textField_3);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(480, 64, 181, 20);
+		contentPane.add(textField_4);
+		
+		JLabel lblRef = new JLabel("Ref.");
+		lblRef.setBounds(449, 68, 46, 14);
+		contentPane.add(lblRef);
 	}
-
 }
